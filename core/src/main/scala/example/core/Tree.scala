c7119a93bda71f9efe0842712dd9592bd76aa49d
Nguyen Lam <nla@dixa.com>
2025-06-26 12:55:14 +0200

    feat: add maximum

--- core/src/main/scala/example/core/Tree.scala
+++ core/src/main/scala/example/core/Tree.scala
@@ -8,4 +8,8 @@ object Tree {
   def size[A](t: Tree[A]): Int = t match
     case Leaf(_) => 1
     case Branch(l, r) => 1 + size(l) + size(r)
+
+  def maximum(t: Tree[Int]): Int = t match
+    case Leaf(value) => value
+    case Branch(l, r) => maximum(l).max(maximum(r))
 }
--- core/src/test/scala/example/core/TreeSuite.scala
+++ core/src/test/scala/example/core/TreeSuite.scala
@@ -11,4 +11,31 @@ class TreeSuite extends munit.FunSuite {
 
     assert(Tree.size(tree) == 3)
   }
+
+  test("Get maximum element of tree") {
+    val left = Leaf(1)
+    val right = Leaf(2)
+
+    val tree = Branch(left, right)
+
+    assert(Tree.maximum(tree) == 2)
+  }
+
+  test("Get maximum element of tree 2") {
+    val left = Leaf(1)
+    val right = Branch(Leaf(10), Leaf(99))
+
+    val tree = Branch(left, right)
+
+    assert(Tree.maximum(tree) == 99)
+  }
+
+  test("Get maximum element of tree 3") {
+    val left = Branch(Branch(Branch(Leaf(4), Leaf(101)), Branch(Leaf(5), Leaf(6))), Leaf(7))
+    val right = Branch(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4))), Leaf(99))
+
+    val tree = Branch(left, right)
+
+    assert(Tree.maximum(tree) == 101)
+  }
 }
