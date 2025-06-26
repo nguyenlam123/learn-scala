62817b119f38b73de2b64415dd4e1441a469832d
Nguyen Lam <nla@dixa.com>
2025-06-26 13:52:04 +0200

    feat: add tree ops using fold

--- core/src/main/scala/example/core/Tree.scala
+++ core/src/main/scala/example/core/Tree.scala
@@ -20,4 +20,16 @@ object Tree {
   def map[A, B](t: Tree[A], f: (a: A) => B): Tree[B] = t match
     case Leaf(value) => Leaf(f(value))
     case Branch(l, r) => Branch(map(l, f),  map(r, f))
+
+  def fold[A, B, C](t: Tree[A], f: (a: A) => B, g: (a: Tree[A], b: Tree[A]) => C) = t match
+    case Leaf(value) => f(value)
+    case Branch(l, r) => g(l, r)
+
+  def foldSize[A](t: Tree[A]): Int = fold(t, (_) => 1, (l: Tree[A], r: Tree[A]) => 1 + foldSize(l: Tree[A]) + foldSize(r: Tree[A]))
+
+  def foldMaximum(t: Tree[Int]): Int = fold(t, (a: Int) => a, (l: Tree[Int], r: Tree[Int]) => foldMaximum(l).max(foldMaximum(r)))
+  
+  def foldDepth[A](t: Tree[A]): Int = fold(t, (_) => 1, (l: Tree[A], r: Tree[A]) => 1 + foldDepth(l).max(foldDepth(r)))
+
+  def foldMap[A, B](t: Tree[A], f: (a: A) => B): Tree[B] = fold(t, (a: A) => Leaf(f(a)), (l: Tree[A], r: Tree[A]) => Branch(foldMap(l, f),  foldMap(r, f)))
 }
--- core/src/test/scala/example/core/TreeSuite.scala
+++ core/src/test/scala/example/core/TreeSuite.scala
@@ -63,4 +63,47 @@ class TreeSuite extends munit.FunSuite {
 
     assert(exp == res)
   }
+
+  test("Get size of tree using fold") {
+    val left = Leaf(1)
+    val right = Leaf(2)
+
+    val tree = Branch(left, right)
+
+    assert(Tree.foldSize(tree) == 3)
+  }
+
+  test("Get maximum element of tree using fold") {
+    val left = Leaf(1)
+    val right = Leaf(2)
+
+    val tree = Branch(left, right)
+
+    assert(Tree.foldMaximum(tree) == 2)
+  }
+
+  test("Get depth of tree using fold") {
+    val left = Leaf(1)
+    val right = Branch(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4))), Leaf(99))
+
+    val tree = Branch(left, right)
+
+    assert(Tree.foldDepth(tree) == 5)
+  }
+
+    test("Map over the elements of tree using fold") {
+    val left = Leaf(1)
+    val right = Branch(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4))), Leaf(99))
+    val tree = Branch(left, right)
+
+    val prod = (a: Int) => a * 2
+
+    val resLeft = Leaf(2)
+    val resRight = Branch(Branch(Branch(Leaf(2), Leaf(4)), Branch(Leaf(6), Leaf(8))), Leaf(198))
+    val res = Branch(resLeft, resRight)
+
+    val exp = Tree.foldMap(tree, prod)
+
+    assert(exp == res)
+  }
 }
