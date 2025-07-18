f7b2605de2b089d76f9d7c2a6e1ff5ddb8311272
Nguyen Lam <nla@dixa.com>
2025-07-18 10:03:13 +0200

    feat: get more producer logs

--- core/src/main/scala/example/core/ProducerConsumer.scala
+++ core/src/main/scala/example/core/ProducerConsumer.scala
@@ -4,11 +4,13 @@ import cats.effect.std.Console
 import cats.syntax.all._
 import scala.collection.immutable.Queue
 import scala.collection.immutable.List
+import scala.concurrent.duration.DurationInt
 
 object ProducerConsumer extends IOApp {
-  def producer[F[_]: Sync: Console](queueR: Ref[F, Queue[Int]], counter: Int): F[Unit] =
+  def producer[F[_]: Async: Console](queueR: Ref[F, Queue[Int]], counter: Int): F[Unit] =
     for {
-      _ <- Sync[F].whenA(counter % 10000 == 0)(Console[F].println(s"Produced $counter items"))
+      _ <- Async[F].whenA(counter % 10000 == 0)(Console[F].println(s"Produced $counter items"))
+      _ <- Async[F].sleep(1.microsecond) // To prevent overwhelming consumers
       _ <- queueR.getAndUpdate(_.enqueue(counter + 1))
       _ <- producer(queueR, counter + 1)
     } yield ()
