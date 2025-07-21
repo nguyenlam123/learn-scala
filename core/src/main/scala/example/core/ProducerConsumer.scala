f423e136a9ec3b0dfe9d3a622a7d71a667443c58
Nguyen Lam <nla@dixa.com>
2025-07-21 08:15:38 +0200

    feat: cancellation

--- core/src/main/scala/example/core/ProducerConsumer.scala
+++ core/src/main/scala/example/core/ProducerConsumer.scala
@@ -5,32 +5,63 @@ import cats.syntax.all._
 import scala.collection.immutable.Queue
 import scala.collection.immutable.List
 import scala.concurrent.duration.DurationInt
+import cats.effect.Deferred
 
 object ProducerConsumer extends IOApp {
-  def producer[F[_]: Async: Console](queueR: Ref[F, Queue[Int]], counter: Int): F[Unit] =
+  case class State[F[_], A](queue: Queue[A], takers: Queue[Deferred[F,A]])
+  object State {
+    def empty[F[_], A]: State[F,A] = State(Queue.empty, Queue.empty)
+  }
+
+  def producer[F[_]: Async: Console](id: Int, counterR: Ref[F, Int], stateR: Ref[F, State[F,Int]]): F[Unit] = {
+
+    def offer(i: Int): F[Unit] =
+      stateR.modify {
+        case State(queue, takers) if takers.nonEmpty =>
+          val (taker, rest) = takers.dequeue
+          State(queue, rest) -> taker.complete(i).void
+        case State(queue, takers) =>
+          State(queue.enqueue(i), takers) -> Async[F].unit
+      }.flatten
+
     for {
-      _ <- Async[F].whenA(counter % 10000 == 0)(Console[F].println(s"Produced $counter items"))
+      i <- counterR.getAndUpdate(_ + 1)
+      _ <- offer(i)
+      _ <- Async[F].whenA(i % 100000 == 0)(Console[F].println(s"Producer $id has reached $i items"))
       _ <- Async[F].sleep(1.microsecond) // To prevent overwhelming consumers
-      _ <- queueR.getAndUpdate(_.enqueue(counter + 1))
-      _ <- producer(queueR, counter + 1)
+      _ <- producer(id, counterR, stateR)
     } yield ()
+  }
 
-  def consumer[F[_]: Sync: Console](queueR: Ref[F, Queue[Int]]): F[Unit] =
-    for {
-      iO <- queueR.modify{ queue =>
-        queue.dequeueOption.fold((queue, scala.None: scala.Option[Int])){case (i,queue) => (queue, scala.Some(i))}
+  def consumer[F[_]: Async: Console](id: Int, stateR: Ref[F, State[F, Int]]): F[Unit] = {
+    val take: F[Int] =
+      Deferred[F, Int].flatMap { taker =>
+        stateR.modify {
+          case State(queue, takers) if queue.nonEmpty =>
+            val (i, rest) = queue.dequeue
+            State(rest, takers) -> Async[F].pure(i) // Got element in queue, we can just return it
+          case State(queue, takers) =>
+            State(queue, takers.enqueue(taker)) -> taker.get // No element in queue, must block caller until some is available
+        }.flatten
       }
-      _ <- Sync[F].whenA(iO.fold(false)(_ % 10000 == 0))(Console[F].println(s"Consumed ${iO.getOrElse(0)} items"))
-      _ <- consumer(queueR)
-    } yield ()
 
-  override def run(args: List[String]): IO[ExitCode] =
     for {
-      queueR <- Ref.of[IO, Queue[Int]](Queue.empty[Int])
-      res <- (consumer(queueR), producer(queueR, 0))
-        .parMapN((_, _) => ExitCode.Success) // Run producer and consumer in parallel until done (likely by user cancelling with CTRL-C)
-        .handleErrorWith { t =>
-          Console[IO].errorln(s"Error caught: ${t.getMessage}").as(ExitCode.Error)
-        }
-    } yield res
+      i <- take
+      _ <- Async[F].whenA(i % 10000 == 0)(Console[F].println(s"Consumer $id has reached $i items"))
+      _ <- consumer(id, stateR)
+    } yield ()
+  }
+
+    override def run(args: List[String]): IO[ExitCode] =
+      for {
+        stateR <- Ref.of[IO, State[IO,Int]](State.empty[IO, Int])
+        counterR <- Ref.of[IO, Int](1)
+        producers = List.range(1, 11).map(producer(_, counterR, stateR)) // 10 producers
+        consumers = List.range(1, 11).map(consumer(_, stateR))           // 10 consumers
+        res <- (producers ++ consumers)
+          .parSequence.as(ExitCode.Success) // Run producers and consumers in parallel until done (likely by user cancelling with CTRL-C)
+          .handleErrorWith { t =>
+            Console[IO].errorln(s"Error caught: ${t.getMessage}").as(ExitCode.Error)
+          }
+      } yield res
 }
